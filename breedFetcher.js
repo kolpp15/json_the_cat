0922ce37e43870a04e553a13b669139382a017de
Brian Sohn <kolpp15@gmail.com>
2021-07-29 16:36:58 -0700

    refactored and tested with M&C

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,31 +1,45 @@
 const request = require('request');
-const breed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search/?q=${breed}`;
 
 
-request(url, (error, response, body) => {
-  console.log(`---------------------------------------------`);
-  console.log('statuscode: ', response.statusCode);
-  console.log(`---------------------------------------------`);
+//                error value                       description value
+// iff success,     null                          the description from body
+// if failute,      error we get from request         null
 
-  if (response.statusCode !== 200) {
-    console.log(`There is no matching breed description!!!`, error);
-  }
+const fetchBreedDescription = function(breedName, callback) {
 
-  const data = JSON.parse(body);
+  const url = `https://api.thecatapi.com/v1/breeds/search/?q=${breedName}`;
+
+  request(url, (error, response, body) => {
+    console.log(`---------------------------------------------`);
+
+    if (response.statusCode !== 200) {
+      console.log('statuscode: ', response.statusCode);
+    } else {
+      console.log('Operation successful!');
+    }
+
+    console.log(`---------------------------------------------`);
   
-  if (response.statusCode === 200) {
-    console.log(`BREED DESCRIPTION:\n  ${data[0].description}`);
-  }
+    if (error) {
+      callback(`@@@@@@@@@@ ERROR ERROR ERROR @@@@@@@@@@`, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(`@@@@@ CHECK THE ABOVE STATUSCODE @@@@@`, null);
+      return;
+    }
   
-  // console.log(`---------------------------------------------`);
-  // console.log(error);
-  // console.log(`---------------------------------------------`);
-  // console.log(response);
-  // console.log(`---------------------------------------------`);
-  // console.log('statuscode:', response.statusCode);
-  
-  
-});
+    const data = JSON.parse(body);
+
+    if (!data[0]) {
+      callback(`@@@@@ SORRY, THERE'S NO MATCHING BREED @@@@@`, null);
+      return;
+    }
+    callback(null, `BREED DESCRIPTION:\n  ${data[0]['description']}`); // don't need if & return
+  });
+};
 
 
+
+module.exports = { fetchBreedDescription };
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,37 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = `BREED DESCRIPTION:\n  The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.`;
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+
+//                error value                       description value
+// iff success,     null                          the description from body
+// if failute,      error we get from request         null
+
+  it('invalid/non-existent breed is passed in', (done) => {
+    fetchBreedDescription('brian', (err, desc) => {
+      const expectedDesc = null;
+
+      // compare returned description
+      assert.equal(expectedDesc, desc);
+
+      done();
+    });
+  });
+
+
+});
